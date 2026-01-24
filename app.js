1cf42b4e06a27013076c33de1c5d966c6e22a789
davidbabaev <davidbabaev198@gmail.com>
2026-01-24 19:20:49 +0200

    add like update delete features

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { createCard, getCards, updateCard, deleteCard } = require('./cards/models/cardsAccessDataService');
+const { createCard, getCards, updateCard, deleteCard, likeCard } = require('./cards/models/cardsAccessDataService');
+const Card = require('./cards/models/mongodb/Card');
 
 const app = express();
 const PORT = 8181;
@@ -33,10 +34,63 @@ app.get('/cards', async (req, res) => {
   }
 });
 
+// GET single card by ID
+app.get('/cards/:id', async (req, res) => {
+  try {
+    let card = await Card.findById(req.params.id);
+    if (!card) {
+      return res.status(404).send('Card not found');
+    }
+    res.send(card);
+  }
+  catch(err) {
+    res.status(400).send(err.message);
+  }
+});
+
 // POST new card
 app.post('/cards', async (req, res) => {
   try {
     let card = await createCard(req.body);
+    res.send(card);
+  }
+  catch(err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// PUT update card
+app.put('/cards/:id', async (req, res) => {
+  try {
+    let card = await updateCard(req.params.id, req.body);
+    if (!card) {
+      return res.status(404).send('Card not found');
+    }
+    res.send(card);
+  }
+  catch(err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// PATCH like/unlike card
+app.patch('/cards/:id', async (req, res) => {
+  try {
+    let card = await likeCard(req.params.id, req.body.userId);
+    res.send(card);
+  }
+  catch(err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// DELETE card
+app.delete('/cards/:id', async (req, res) => {
+  try {
+    let card = await deleteCard(req.params.id);
+    if (!card) {
+      return res.status(404).send('Card not found');
+    }
     res.send(card);
   }
   catch(err) {
@@ -49,53 +103,3 @@ app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
   connectToDB();
 });
-
-
-
-
-
-
-
-
-
-/* const express = require('express');
-const mongoose = require('mongoose');
-const {createCard, getCards, updateCard, deleteCard} = require('./cards/models/cardsAccessDataService')
-const app = express();
-app.use(express.json());
-
-const Card = require('../mongoDBnodeJsFirst/cards/models/mongodb/Card')
-
-const PORT = 8181;
-
-app.get('/cards', (req, res) => {
-  res.send('coonected to app')
-});
-
-const getCard = async (cardId) => {
-  try {
-    let card = await Card.findById(cardId);
-    return card;
-  } catch (error) {
-    throw new Error("Mongoose " + error.message);
-  }
-};
-
-const connectToDB = async () => {
-  try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/cardsServer')
-    console.log('connected to DB');
-  }
-  catch(err){
-    console.log(err.message);
-  }
-}
-
-app.post('/cards', (req, res) => {
-  res.send(Card)
-})
-
-app.listen(PORT, () => {
-  console.log('listing to ', PORT);
-  connectToDB(); // <- add this otherwise you're never connecting to mongoDB
-}) */
--- cards/models/cardsAccessDataService.js
+++ cards/models/cardsAccessDataService.js
@@ -37,5 +37,29 @@ const deleteCard = async (cardId) => {
   }
 };
 
+// NEW: Like/Unlike toggle
+const likeCard = async (cardId, userId) => {
+  try {
+    let card = await Card.findById(cardId);
+    if (!card) {
+      throw new Error("A card with this ID cannot be found in the database");
+    }
+    
+    if (card.likes.includes(userId)) {
+      // User already liked → Remove the like
+      let newLikesArray = card.likes.filter((id) => id != userId);
+      card.likes = newLikesArray;
+    } else {
+      // User hasn't liked → Add the like
+      card.likes.push(userId);
+    }
+    
+    await card.save();
+    return card;
+  } catch (error) {
+    throw new Error("Mongoose " + error.message);
+  }
+};
 
-module.exports = { createCard, getCards, updateCard, deleteCard };
+
+module.exports = { createCard, getCards, updateCard, deleteCard , likeCard};
